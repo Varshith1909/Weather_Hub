41faf9a11b48ffe3941a10f6dc0a90f3f60c5ba7
Varshith1909 <vc8@pdx.edu>
2023-11-09 12:57:59 -0800

    Added Login, navbar, home functionality

--- weather_hub/src/App.js
+++ weather_hub/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import homepage from './pages/homepage';
 import './App.css';
-<<<<<<< HEAD
 import Login from './Login';
-=======
 import Navbar from './components/navbar';
 import Home from './pages/homepage'
->>>>>>> c0b12da (Navbar and homepage - 1)
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="App-header">
         <Login />
       </header>
-=======
       <Navbar />
       <Home />
->>>>>>> c0b12da (Navbar and homepage - 1)
     </div>
   );
 }
