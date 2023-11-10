90322140d93a74658b4d00713b7f1fb96d7d4845
Harsha0526 <hgattu@pdx.edu>
2023-11-09 23:18:38 -0800

    written testcases for login funtionality

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
