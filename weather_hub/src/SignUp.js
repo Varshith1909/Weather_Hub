7fae9dc500e8cdd2ede6fad678363c9281012016
Harsha0526 <hgattu@pdx.edu>
2023-11-28 18:08:08 -0800

    added sign up page route in login page

---
+++ weather_hub/src/SignUp.js
@@ -0,0 +1,40 @@
+// SignUp.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', username: '', password: '', confirmPassword: '' });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/register', formData);
+      // Handle success - maybe redirect to login or do something else
+      console.log('User registered successfully!!!', response.data);
+    } catch (error) {
+      console.error('Error signing up:', error.response ? error.response.data : error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className='signup-div'>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
+        <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+        <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="Username" />
+        <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" />
+        <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} placeholder="Confirm Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
